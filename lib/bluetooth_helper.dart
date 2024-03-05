288a6c3619dff7d3c70ed69c431bd7195898cf43
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-03-05 22:28:20 +0100

    Create bluetooth_helper.dart

---
+++ lib/bluetooth_helper.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+
+class BluetoothHelper extends ChangeNotifier {
+  FlutterBluetoothSerial bluetooth = FlutterBluetoothSerial.instance;
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+
+  BluetoothState get currentBluetoothState {
+      return _bluetoothState;
+  }
+
+  void updateState() async {
+    _bluetoothState = await bluetooth.state;
+    notifyListeners();
+  }
+}
