1840742e46c5f336c0e239c0a5f6b10cfd379702
Niklas D <80592781+Niklas-gif@users.noreply.github.com>
2024-03-07 12:27:43 +0100

    created Widgets

--- lib/connect_screen.dart
+++ lib/connect_screen.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_arduino_bluetooth_example/bluetooth_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 class ConnectScreen extends StatelessWidget {
   const ConnectScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    BluetoothProvider bluetoothProvider = Provider.of<BluetoothProvider>(context);
+    bluetoothProvider.checkState();
+    if (bluetoothProvider.currentBluetoothState == BluetoothState.STATE_ON) {
+      return const ConnectWidget();
+    } else {
+      return const BluetoothDisabled();
+    }
+  }
+}
+
+class ConnectWidget extends StatelessWidget {
+  const ConnectWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Icon(
+              size: 69 * 6,
+              color: Colors.lightBlueAccent,
+              Icons.bluetooth)
+        ],
+      ),
+    );
+  }
+}
+
+class BluetoothDisabled extends StatelessWidget {
+  const BluetoothDisabled({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Icon(size: 69*6,color: Colors.lightBlueAccent, Icons.bluetooth_disabled),
+        child: Icon(
+            size: 69 * 6,
+            color: Colors.lightBlueAccent,
+            Icons.bluetooth_disabled),
       ),
     );
   }
